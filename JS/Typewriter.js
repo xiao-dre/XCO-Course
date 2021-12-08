6d61c00d23d48244d337ade99662a432d98dc809
Andreas Alexander <72374364+xiao-dre@users.noreply.github.com>
2021-12-08 09:39:50 +0700

    Add files via upload

---
+++ JS/Typewriter.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.animate', {
+    strings: [
+        "XCO Coding Course",
+        "XCO Coding Course",
+        "XCO Coding Course",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  });
